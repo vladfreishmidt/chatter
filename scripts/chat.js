c299a636a380340516ca18b7c76ecafa3fcf4569
vladfreishmidt <vladfreishmidt.webdevelopment@gmail.com>
2022-12-16 13:04:32 +0200

    Add unsub

--- scripts/chat.js
+++ scripts/chat.js
@@ -8,6 +8,7 @@ class Chatroom {
     this.room = room;
     this.username = username;
     this.chats = db.collection('chats');
+    this.unsub;
   }
 
   async addChat(message) {
@@ -25,7 +26,7 @@ class Chatroom {
   }
 
   getChats(callback) {
-    this.chats
+    this.unsub = this.chats
       .where('room', '==', this.room)
       .orderBy('created_at')
       .onSnapshot((snapshot) => {
@@ -39,16 +40,26 @@ class Chatroom {
   }
 
   updateName(username) {
-    this.user = username;
+    this.username = username;
   }
 
   updateRoom(room) {
     this.room = room;
+    console.log('room updated');
+    if (this.unsub) {
+      this.unsub();
+    }
   }
 }
 
 const chatroom = new Chatroom('general', 'shaun');
 
-chatroom.getChats((data) => {
-  console.log(data);
-});
+setTimeout(() => {
+  chatroom.updateRoom('gaming');
+  chatroom.updateName('Yoshi');
+  chatroom.getChats((data) => {
+    console.log(data);
+  });
+
+  chatroom.addChat('hello');
+}, 3000);
